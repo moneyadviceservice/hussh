f72cdaedbc269eeb206f5d10cb507e8754dea544
Misha Gorodnitzky <misha.gorodnitzky@moneyadviceservice.org.uk>
2015-04-07 17:19:53 +0100

    Mallory now works with Net::SSH callbacks.
    Net::SSH has two ways of operating, immediate mode using exec! and
    callback-mode using Net::SSH::Connection::Session#open_channel and
    Net::SSH::Connection::Channel::#on_data, etc. Mallory now works with simple
    cases of the callback-mode, more functionality will be added later.

--- lib/mallory.rb
+++ lib/mallory.rb
@@ -6,15 +6,16 @@ module Net
   module SSH
     class << self
       def start_with_mallory(host, user, options = {}, &block)
-        if Mallory::Responses.has_recording? || Mallory.allow_connections?
-          session = Mallory::Session.new(host, user)
-          if block_given?
-            yield session
-            session.close
-          else
-            session
-          end
+        # TODO: Let's do this later once everything else is working.
+        # if Mallory.has_recording? || Mallory.allow_connections?
+        session = Mallory::Session.new(host, user)
+        if block_given?
+          yield session
+          session.close
+        else
+          session
         end
+        # end
       end
       alias_method :start_without_mallory, :start
       alias_method :start, :start_with_mallory
@@ -23,17 +24,32 @@ module Net
 end
 
 
+def mattr_accessor(*syms)
+  syms.each do |sym|
+    class_eval(<<EOD)
+@@#{sym} = nil unless defined? @@#{sym}
+def self.#{sym}
+  @@#{sym}
+end
+
+def self.#{sym}=(obj)
+  @@#{sym} = obj
+end
+EOD
+  end
+end
+
 module Mallory
   def self.allow_connections?
-    @allow_connections ||= false
+    @@allow_connections ||= false
   end
 
-  def self.allow_connections
-    @allow_connections = true
+  def self.allow_connections!
+    @@allow_connections = true
   end
 
   def self.disallow_connections
-    @allow_connections = false
+    @@allow_connections = false
   end
 
   def self.configure
@@ -41,25 +57,74 @@ module Mallory
   end
 
   def self.configuration
-    @configuration ||= Configuration.new
+    @@configuration ||= Configuration.new
   end
 
   def self.commands_run
     @@commands_run ||= []
   end
 
+  @@recordings_directory = 'fixtures/mallory'
+  mattr_accessor :recordings_directory
+  mattr_accessor :stubbed_responses, :recorded_responses
+
+  def self.has_recording?
+    !!@@recorded_responses
+  end
+
+  def self.load_recording(name)
+    @@recording_path = self.path_for_recording(name)
+    @@recording_changed = false
+    if File.exist?(@@recording_path)
+      @@recorded_responses = YAML.load_file(@@recording_path)
+    else
+      self.clear_recorded_responses
+    end
+  end
+
+  def self.save_recording_if_changed
+    return if !self.recording_changed?
+    @@recording_path ||= self.path_for_recording(name)
+    if !File.exist?(@@recording_path)
+      FileUtils.mkdir_p(File.dirname(@@recording_path))
+    end
+    File.write(@@recording_path, @@recorded_responses.to_yaml)
+    @@recording_changed = false
+  end
+
+  def self.clear_recorded_responses
+    @@recorded_responses = {}
+  end
+
+  def self.recording_changed?
+    @@recording_changed
+  end
+
+  def self.recording_changed!
+    @@recording_changed = true
+  end
+
+  def self.clear_stubbed_responses
+    @@stubbed_responses = {}
+  end
+
+  # private
+  def self.path_for_recording(name)
+    "#{@@recordings_directory}/#{name}.yaml"
+  end
+
   class Configuration
     def recordings_directory(directory)
-      Mallory::Responses.recordings_directory = directory
+      Mallory.recordings_directory = directory
     end
 
     def configure_rspec
       ::RSpec.configure do |config|
         recording_name_for = lambda do |metadata|
-          if metadata.has_key? :parent_example_group
+          if metadata.has_key?(:parent_example_group)
             recording_name_for[metadata[:parent_example_group]] +
               metadata[:description]
-          elsif metadata.has_key? :example_group
+          elsif metadata.has_key?(:example_group)
             recording_name_for[metadata[:example_group]] +
               metadata[:description]
           else
@@ -72,16 +137,15 @@ module Mallory
           options = options.is_a?(Hash) ? options.dup : {}
           recording_name = options.delete(:recording_name) ||
                            recording_name_for[example.metadata]
-          Responses.load_recording(recording_name)
+          Mallory.load_recording(recording_name)
+          Mallory.clear_stubbed_responses
         end
 
         config.after(:each, mallory: lambda { |v| !!v }) do |example|
           options = example.metadata[:mallory]
           options = options.is_a?(Hash) ? options.dup : {}
-          recording_name = options.delete(:recording_name) ||
-                           recording_name_for[example.metadata]
-          Responses.save_recording recording_name
-          Responses.recording.clear
+          Mallory.clear_recorded_responses
+          Mallory.clear_stubbed_responses
           Mallory.commands_run.clear
         end
       end
@@ -92,35 +156,105 @@ module Mallory
     def initialize(host, user)
       @host = host
       @user = user
-      @responses = Responses.responses_for_host_and_user(host, user)
-      @recording = Responses.recording[host][user]
     end
 
     def real_session
       @real_session ||= Net::SSH.start_without_mallory(@host, @user)
     end
 
-    def responses
-      @responses
+    def has_response?(command)
+      Mallory.stubbed_responses.fetch(@host, {}).fetch(@user, {})
+        .has_key?(command) ||
+        Mallory.recorded_responses.fetch(@host, {}).fetch(@user, {})
+        .has_key?(command)
+    end
+
+    def response_for(command)
+      Mallory.stubbed_responses.fetch(@host, {}).fetch(@user, {}).fetch(
+        command,
+        Mallory.recorded_responses.fetch(@host, {}).fetch(@user, {})[command]
+      )
+    end
+
+    def update_recording(command, response)
+      Mallory.recorded_responses[@host] ||= {}
+      Mallory.recorded_responses[@host][@user] ||= {}
+      Mallory.recorded_responses[@host][@user][command] = response
+      Mallory.recording_changed!
     end
 
     def exec!(command)
       Mallory.commands_run << command
-      if @responses.has_response? command
-        @responses[command]
+      if self.has_response?(command)
+        self.response_for(command)
       else
-        response = real_session.exec! command
-        Responses.recording_changed = true
-        @recording[command] = response
+        response = real_session.exec!(command)
+        self.update_recording(command, response)
       end
     end
 
     def open_channel(&block)
       @channel = Channel.new(self)
       block.call(@channel)
-      @channel.close
-      @recording[@channel.command_executed] = @channel.response_data
-      Responses.recording_changed = true
+      Mallory.commands_run << @channel.command
+      if self.has_response?(@channel.command)
+        if @channel.exec_block.respond_to?(:call)
+          @channel.exec_block.call(@channel, true)
+        end
+
+        if @channel.on_data_block.respond_to?(:call)
+          @channel.on_data_block.call(@channel, self.response_for(@channel.command))
+        end
+      else
+        self.real_session.open_channel do |ch|
+          ch.exec(@channel.command) do |ch, success|
+            if @channel.exec_block.respond_to?(:call)
+              @channel.exec_block.call(@channel, success)
+            end
+          end
+
+          if @channel.on_data_block.respond_to?(:call)
+            ch.on_data do |ch, output|
+              @channel.on_data_block.call(@channel, output)
+              @on_data = output
+              # TODO: Move below logic into update_recording?
+              # if !@on_data
+              #   @on_data = output
+              # else
+              #   @on_data = [@on_data] if !@on_data.is_a? Array
+              #   @on_data << output
+              # end
+              self.update_recording(@channel.command, @on_data)
+            end
+          end
+
+          ch.on_extended_data do |ch, output|
+            if @channel.on_extended_data_block.respond_to?(:call)
+              @channel.on_extended_data_block.call(@channel, output)
+            end
+
+            # TODO: We don't have a way to record this yet.
+            # if !@on_extended_data
+            #   @on_extended_data = output
+            # else
+            #   if !@on_extended_data.is_a? Array
+            #     @on_extended_data = [@on_extended_data]
+            #   end
+            #   @on_extended_data << output
+            # end
+          end
+
+          ch.request_pty do |ch, success|
+            # TODO: We need a way to record this
+            if @channel.request_pty_block.respond_to?(:call)
+              @channel.request_pty_block.call(ch, success)
+            end
+          end
+
+          # TODO: Should wait be here or should it be in the calling code?
+          ch.wait
+        end
+      end
     end
 
     def close
@@ -143,144 +277,40 @@ module Mallory
       @data
     end
 
-    def command_executed
-      @command
+    attr :command
+    attr :exec_block
+    def exec(command, &block)
+      @command = command
+      @exec_block = block
     end
 
+    attr :on_data_block
+    def on_data(&block)
+      @on_data_block = block
+    end
+
+    attr :on_extended_data_block
+    def on_extended_data(&block)
+      @on_extended_data_block = block
+    end
+
+    attr :request_pty_block
     def request_pty(&block)
       @request_pty = true
       @request_pty_block = block
     end
 
-    def exec(command, &block)
-      Mallory.commands_run << @command = command
-      if block_given?
-        real_channel.exec(command) do |ch, success|
-          block.call(self, success)
-        end
-      end
+    def requested_pty?
+      @request_pty
     end
 
-    def close
-      @real_channel.close if @real_channel
-    end
+    # def on_close(&block)
+    #   @on_close = block
+    # end
 
-    def on_data(&block)
-      real_channel.on_data do |ch, data|
-        @data ||= ''
-        @data += data
-        block.call(ch, data)
-      end
-    end
-
-    def on_extended_data(&block)
-      real_channel.on_extended_data do |ch, data|
-        @extended_data ||= []
-        @extended_data << data
-        block.call(ch, data)
-      end
-    end
-
-    def request_pty(&block)
-      if block_given?
-        real_channel.request_pty do |ch, success|
-          @request_pty_success = success
-          block.call(ch, success)
-        end
-      else
-        real_channel.request_pty
-      end
-    end
-
-    def on_close(&block)
-      @on_close = block
-    end
-  end
-
-  class Responses
-    @@recording = {}
-    @@recording_changed = false
-    @@recordings_directory = 'fixtures/mallory'
-
-    def self.recordings_directory=(directory)
-      @@recordings_directory = directory
-    end
-
-    def self.recording_path(name)
-      "#{@@recordings_directory}/#{name}.yaml"
-    end
-
-    def self.recording
-      @@recording
-    end
-
-    def self.recording=(responses)
-      @@recording = responses
-    end
-
-    def self.has_recording?
-      !!@@recording
-    end
-
-    def self.recording_changed?
-      @@recording_changed
-    end
-
-    def self.recording_changed=(new_value)
-      @@recording_changed = new_value
-    end
-
-    def self.responses_for_host_and_user(host, user)
-      @@recording[host] ||= {}
-      @@recording[host][user] ||= {}
-      Responses.new(host, user)
-    end
-
-    def self.register_response(host, user, command, response = nil, &block)
-      @@recording[host] ||= {}
-      @@recording[host][user] ||= {}
-      @@recording[host][user][command] = block_given? ? block : response
-    end
-
-    def self.load_recording(name)
-      path = recording_path(name)
-      if File.exist? path
-        self.recording = YAML.load_file path
-      else
-        self.recording = {}
-      end
-      self.recording_changed = false
-    end
-
-    def self.save_recording name
-      path = recording_path(name)
-      if self.recording_changed?
-        if !File.exist? path
-          FileUtils.mkdir_p(File.dirname(path))
-        end
-        File.write(path, recording.to_yaml)
-        self.recording_changed = false
-      end
-    end
-
-    def initialize(host, user)
-      @host = host
-      @user = user
-      @responses = {}
-    end
-
-    def has_response?(command)
-      @responses.has_key?(command) || \
-        @@recording[@host][@user].has_key?(command)
-    end
-
-    def [](command)
-      @responses.fetch(command, @@recording[@host][@user][command])
-    end
-
-    def []=(command, value)
-      @responses[command] = value
-    end
+    # def close
+    #   @real_channel.close if @real_channel
+    # end
   end
 end
 
--- spec/mallory_spec.rb
+++ spec/mallory_spec.rb
@@ -17,7 +17,9 @@ RSpec.describe Mallory do
         spy
       end
       before do
-        Mallory.instance_eval { @commands_run = [] }
+        Mallory.commands_run.clear
+        Mallory.clear_recorded_responses
+        Mallory.clear_stubbed_responses
         allow(Net::SSH).to receive(:start_without_mallory)
                             .and_return(real_session)
       end
@@ -36,12 +38,12 @@ RSpec.describe Mallory do
         end
 
         it 'saves the result of the command' do
-          expect(Mallory::Responses.recording['host']['user']['hostname'])
+          expect(Mallory.recorded_responses['host']['user']['hostname'])
             .to eql('hostname output')
         end
 
         it 'flags the recording as changed' do
-          expect(Mallory::Responses.recording_changed?).to eql(true)
+          expect(Mallory.recording_changed?).to eql(true)
         end
       end
 
@@ -54,7 +56,7 @@ RSpec.describe Mallory do
               'host' => { 'user' => { 'hostname' => "subsix\n" } }
             }.to_yaml
           )
-          Mallory::Responses.load_recording('saved_responses')
+          Mallory.load_recording('saved_responses')
           Net::SSH.start('host', 'user') { |s| s.exec!('hostname') }
         end
 
@@ -67,7 +69,7 @@ RSpec.describe Mallory do
         end
 
         it "doesn't flags the recording as changed" do
-          expect(Mallory::Responses.recording_changed?).to eql(false)
+          expect(Mallory.recording_changed?).to eql(false)
         end
       end
     end
@@ -75,7 +77,9 @@ RSpec.describe Mallory do
 
   describe Mallory::Channel do
     before do
-      Mallory.instance_eval { @commands_run = [] }
+      Mallory.commands_run.clear
+      Mallory.clear_recorded_responses
+      Mallory.clear_stubbed_responses
     end
 
     let!(:channel) do
@@ -83,9 +87,9 @@ RSpec.describe Mallory do
       # Setup a fake Channel object which will hopefully behave like a
       # Net::SSH channel, i.e. register the command and callbacks, and then
       # call them all with the appropriate data and values.
-      allow(channel).to receive(:exec) do |cmd, &block|
+      allow(channel).to receive(:exec) do |cmd, &blk|
         @command = cmd
-        @exec = block
+        blk.call(channel, !@command.match(/-fail$/))
       end
       allow(channel).to receive(:request_pty) { |&block| @request_pty = block }
       allow(channel).to receive(:on_data) { |&block| @on_data = block }
@@ -103,80 +107,108 @@ RSpec.describe Mallory do
           @on_extended_data.call(channel, "#{@command} stderr output")
         end
       end
+      allow(channel).to receive(:wait) do
+        channel.close
+      end
       channel
     end
 
     let!(:session) do
       session = instance_spy('Net::SSH::Connection::Session')
       # The session here is just used to stub our channel in for the real one.
-      allow(session).to receive(:open_channel).and_return(channel)
+      allow(session).to receive(:open_channel).and_yield(channel)
       allow(Net::SSH).to receive(:start_without_mallory).and_return(session)
       session
     end
 
-    context 'when using exec with a block' do
-      context 'the exec succeeded' do
-        before do
-          # Simulate how we would use Mallory, which sits between the
-          # application code (the code below) and the mocked-out Net::SSH
-          # code.
-          Net::SSH.start('host', 'user') do |ssh|
-            ssh.open_channel do |ch|
-              ch.exec 'test' do |ch, success|
-                @exec_success = success
-                ch.request_pty
-                ch.on_data          { |c, data| @data = data }
-                ch.on_extended_data { |c, data| @extended_data = data }
-              end
+    context 'when using an exec block' do
+      before do
+        FileUtils.mkdir_p 'fixtures/mallory'
+        File.write('fixtures/mallory/saved_responses.yaml', saved_responses)
+        Mallory.load_recording('saved_responses')
+
+        # Simulate how we would use Mallory, which sits between the
+        # application code (the code below) and the mocked-out Net::SSH
+        # code.
+        Net::SSH.start('host', 'user') do |session|
+          session.open_channel do |ch|
+            ch.request_pty
+            ch.exec command do |ch, success|
+              @exec_success = success
+              ch.on_data          { |c, data| @data = data }
+              ch.on_extended_data { |c, data| @extended_data = data }
             end
           end
         end
-
-        it 'gives us the stdout' do
-          expect(@data).to eq 'test output'
-        end
-
-        it 'gives us the stderr' do
-          expect(@extended_data).to eq 'test stderr output'
-        end
-
-        it 'allows us to request a pty' do
-          expect(channel).to have_received(:request_pty)
-        end
-
-        it 'gives us the status of the exec' do
-          expect(@exec_success).to eq true
-        end
-
-        it 'records that the command was run' do
-          expect(Mallory.commands_run).to include('test')
-        end
-
-        it 'saves the result of the command' do
-          expect(Mallory::Responses.recording['host']['user']['test'])
-            .to eq 'test output'
-        end
-
-        it 'flags the recording as changed' do
-          expect(Mallory::Responses.recording_changed?).to eql(true)
-        end
       end
 
-      context 'the exec has failed' do
-        before do
-          Net::SSH.start('host', 'user') do |ssh|
-            ssh.open_channel do |ch|
-              ch.exec('test-fail') { |ch, success| @exec_success = success }
-            end
+      context 'with a command that has not been run before' do
+        let(:saved_responses) { {}.to_yaml }
+
+        context 'and execution is succesful' do
+          let(:command) { 'test' }
+
+          it 'runs the command via ssh' do
+            expect(channel).to have_received(:exec).with('test')
+          end
+
+          it 'records that the command was run' do
+            expect(Mallory.commands_run).to include('test')
+          end
+
+          it 'passes command status to exec' do
+            expect(@exec_success).to eql(true)
+          end
+
+          it 'gives us the stdout' do
+            expect(@data).to eq 'test output'
+          end
+
+          it 'gives us the stderr' do
+            expect(@extended_data).to eq 'test stderr output'
+          end
+
+          it 'allows us to request a pty' do
+            expect(channel).to have_received(:request_pty)
+          end
+
+          it 'saves the result of the command' do
+            expect(Mallory.recorded_responses['host']['user']['test'])
+              .to eq 'test output'
+          end
+
+          it 'flags the recording as changed' do
+            expect(Mallory.recording_changed?).to eql(true)
           end
         end
-        subject { @exec_success }
-        it { is_expected.to eq false }
+
+        context 'and has failed to execute' do
+          let(:command) { 'test-fail' }
+          subject { @exec_success }
+          it { is_expected.to eq false }
+        end
       end
 
+      context 'with a command that has recorded results' do
+        let(:saved_responses) do
+          {
+            'host' => { 'user' => { 'test' => 'recorded test output' } }
+          }.to_yaml
+        end
 
+        context 'and execution is succesful' do
+          let(:command) { 'test' }
+
+          it 'gives us the stdout' do
+            expect(@data).to eq 'recorded test output'
+          end
+
+          it 'gives us the success status' do
+            expect(@exec_success).to eql(true)
+          end
+        end
+      end
     end
-    context 'when using exec without a block'
   end
 end
 
